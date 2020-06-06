e649cf473d71455bf458db4fcbb12659d0ea836b
lstepany <lstepany@hive.helsinki.fi>
2020-06-06 10:40:48 +0200

    adding memset

---
+++ tests/main_memset.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void	*ft_memset(char *s, int c, size_t n);
+
+int main(int argc, char **argv)
+{
+
+	int n;
+	int i;
+
+	i = 0;
+	n = 0;
+   	while(argv[3][i])
+	{
+		n = 10 * n + argv[3][i] - 48;
+		i++;
+	}
+	ft_memset(argv[1], argv[2][0], n );
+	printf("%s\n", argv[1]);
+	return(0);
+}
