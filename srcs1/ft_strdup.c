0bbacaf71a2ff23309ea01648bd2638c0b47274b
lstepany <lstepany@hive.helsinki.fi>
2020-06-06 21:55:29 +0200

    adding more

---
+++ srcs1/ft_strdup.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lstepany <lstepany@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/03 18:25:41 by lstepany          #+#    #+#             */
+/*   Updated: 2020/06/04 14:20:33 by lstepany         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int		ft_strlen(char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	char	*res;
+
+	res = (char*)malloc((ft_strlen(src) + 1) * sizeof(char));
+	if (res == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (src[i])
+	{
+		res[i] = src[i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
