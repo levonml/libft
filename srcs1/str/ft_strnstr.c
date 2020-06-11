3038464f05414ce56057a36f23ce436e20db5bda
lstepany <lstepany@hive.helsinki.fi>
2020-06-11 16:41:38 +0200

    adding ap-to atoi

---
+++ srcs1/str/ft_strnstr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lstepany <lstepany@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/11 12:38:08 by lstepany          #+#    #+#             */
+/*   Updated: 2020/06/11 13:37:57 by lstepany         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+char *ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t i;
+	size_t k;
+	int j;
+
+		if ((char*)little == NULL)
+			return ((char*)big);
+		else
+		{
+			k = 0;
+			while (big[k] && (k < len))
+			{
+				j = 0;
+				i = k;
+				while ((little[j]) && (big[i] == little[j]) && (i < len))
+				{
+					j++;
+					i++;
+				}
+				if (little[j] == '\0')
+					return ((char*)(big + k));
+				else
+					k++;
+			}
+		}
+	return (NULL);
+}
