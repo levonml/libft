2d9578e205250c87edbff9ac195806f953330390
lstepany <lstepany@hive.helsinki.fi>
2020-06-27 11:21:13 +0200

    ft_atoi is fixed

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: lstepany <lstepany@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/11 16:05:29 by lstepany          #+#    #+#             */
-/*   Updated: 2020/06/27 09:20:42 by lstepany         ###   ########.fr       */
+/*   Updated: 2020/06/27 10:59:23 by lstepany         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,15 @@ int	ft_atoi(const char *nptr)
 	i = 0;
 	res = 0;
 	s = 1;
-	if (nptr[0] == '-')
+	while (nptr[i] == ' ' || nptr[i] == '\r' || nptr[i] == '\v' || nptr[i] == '\t' || nptr[i] == '\n')
+		i++;
+	if (nptr[i] == '-')
 	{
-		i = 1;
+		i++;
 		s = -1;
 	}
-	if (nptr[0] == '+')
-		i = 1;
+	if (nptr[i] == '+')
+		i++;
 	while (nptr[i] <= '9' && nptr[i] >= '0')
 	{
 		res = res * 10 + nptr[i] - '0';
