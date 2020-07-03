85b8fbc04b2b00f55c36b06121e4421bc55219bb
lstepany <lstepany@student.hive.fi>
2020-07-03 19:33:00 +0300

    adding some lst

--- tests_libft/ft_lstdelone_main.c
+++ tests_libft/ft_lstdelone_main.c
@@ -1,34 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*   ft_lstdelone_main.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lstepany <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/07/02 10:10:16 by lstepany          #+#    #+#             */
-/*   Updated: 2020/07/03 18:47:34 by lstepany         ###   ########.fr       */
+/*   Created: 2020/07/03 18:50:38 by lstepany          #+#    #+#             */
+/*   Updated: 2020/07/03 19:26:49 by lstepany         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include "libft.h"
-#include <stdlib.h>
-
-void ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
+void del(void *str, size_t n)
 {
-/*	t_list *lst;
-
-	lst = *alst;
-	if (lst != NULL)
-	{
-		del(lst -> content, lst -> content_size);	
-		free(lst);
-	}
-	lst = NULL;*/
-	if (*alst != NULL)
-	{
-		del((*alst) -> content, (*alst) -> content_size);	
-	free(*alst);
-	}
-	*alst = NULL;
+	ft_bzero(str ,n);
 }
+int main(void)
+{
+	t_list *test;
 
+	test = ft_lstnew("test", 5);
+	ft_lstdel(test,  &del("test", 5));
+	printf("content = %s\nsize = %zu", test -> content, test -> content_size);
+	return(0);
+}
